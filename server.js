4073f4fcca32b79c30f75706aa5f7f2c14326795
Yoga Cats <esherrow77@gmail.com>
2021-06-21 16:35:32 -0400

    add initial query

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const express = require('express');
+const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 
-app.get('/', (req, res)=>{
-    res.json({
-        message: 'Hello World'
-    });
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'C0ff33&c4t5',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows)=>{
+    console.log(rows);
 });
 
 app.use((req, res)=>{
